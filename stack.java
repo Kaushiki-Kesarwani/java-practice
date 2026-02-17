1b9987bc3bbf18b418fc479d54217d34564bcaf8
mahak <kaushikikes@gmail.com>
2026-02-17 23:27:37 +0530

    function call

--- stack.java
+++ stack.java
@@ -48,9 +48,12 @@ public class stack {
         s.push(1);
         s.push(2);
         s.push(4);
+        s.push(5);
+        s.push(8);
 
         System.out.println(s.peek()); // 4
         System.out.println(s.pop());  // 4
         System.out.println(s.peek()); // 2
+
     }
 }
