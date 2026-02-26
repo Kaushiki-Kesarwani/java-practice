2293ab600a2bcbdcd9daefcb529921116b5201d8
mahak <kaushikikes@gmail.com>
2026-02-26 19:15:22 +0530

    circular array using java

---
+++ circular_queue_using_array.java
@@ -0,0 +1,81 @@
+public class circular_queue_using_array {
+    
+    static class Queue{
+        static int arr[];
+        static int size;
+        static int rear = -1;
+        static int front = -1;
+
+        Queue(int n){
+            arr = new int[n];
+            this.size = n;
+        }
+
+        public static boolean isEmpty(){
+            return rear == -1 && front ==-1;
+        }
+
+        public static boolean isFull(){
+            return (rear+1)%size == front;
+        }
+
+        //enqueue
+        public static void add(int data){
+            if(isFull()){
+                System.out.println("Queue is full");
+                return;
+            }
+            if(front == -1){
+                front = 0;
+            }
+            rear = (rear+1)%size;
+            arr[rear]=data;
+        }
+
+        //dequeue
+        public static int remove(){
+            if(isEmpty()){
+                return -1;
+            }
+
+         int res = arr[front];
+        if(rear == front){
+            rear = front = -1;
+        }
+            else{
+                front = (front + 1) % size;
+            }
+            return res;
+        }
+
+        //peek
+        public static int peek(){
+             if(isEmpty()){
+                return -1;
+            }
+
+            return arr[front];
+
+        }
+        
+    }
+
+    public static void main(String args[]){
+        Queue q1 = new Queue(6) ;
+        q1.add(1);
+          q1.add(2);
+            q1.add(3);
+              q1.add(4);
+                q1.add(5);  
+                 q1.add(8);  
+                while(!q1.isEmpty()) {
+                    System.out.println(q1.peek());
+                    q1.remove();
+                    
+                }
+               
+                q1.add(9);
+                System.out.println("peek element : "+q1.peek());
+    }
+}
+
