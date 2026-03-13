608ab253fae46d471dbbc89ac29f54e2b647a5e6
mahak <kaushikikes@gmail.com>
2026-03-13 23:11:31 +0530

    set bit

---
+++ bitmaskSet.java
@@ -0,0 +1,12 @@
+class BitmaskSet {
+    public static void main(String args[]){
+   int n = 9;
+   int pos = 1;
+   int bitmask = 1 << pos;
+
+int newNumber = bitmask | n;
+System.out.println("new Number : "+ newNumber);
+    }
+   
+
+    }
